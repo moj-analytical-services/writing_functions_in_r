4e1ab36d4c05251f30f0eec7a7ec06d3d3b9e745
phillip.buckham-bonnett@justice.gov.uk <phillip.buckham-bonnett@justice.gov.uk>
2023-11-28 02:15:47 +0000

    Render straight to the correct files

--- rmd_files/render_rmarkdown_files.R
+++ rmd_files/render_rmarkdown_files.R
@@ -1,31 +1,44 @@
 # Render markdowns
 
-# render readme.Rmd as GitHub markdown document
-rmarkdown::render("rmd_files/README.Rmd", output_format = "github_document")
+### Render README.Rmd as GitHub markdown document --------------------------------------------------
+# Note: this includes rmd_files/content.Rmd and rmd_files/appendix.Rmd.
+rmarkdown::render(
+  "rmd_files/README.Rmd", 
+  output_format = "github_document",
+  output_dir = "../",
+  output_file = file.path("../README.md")
+)
 
-# render slides.Rmd as an ioslides presentation
-rmarkdown::render("rmd_files/slides.Rmd", output_format = "ioslides_presentation")
+### Render slides.html as an isoslides presentation ------------------------------------------------
+# Note: this includes rmd_files/content.Rmd.
+rmarkdown::render(
+  "rmd_files/slides.Rmd", 
+  output_format = "ioslides_presentation",
+  output_dir = "../",
+  output_file = file.path("../slides.html")
+)
 
-# move rendered files to main directory
-file.rename("rmd_files/README.md", "README.md")
-file.remove("rmd_files/README.html")
-file.rename("rmd_files/slides.html", "slides.html")
 
-# extract R code from content.Rmd into separate script
-purl_solutions <- FALSE # This means solutions won't be included
-purl_example_code <- TRUE # This means all code apart from solutions will be included
-knitr::purl("rmd_files/content.Rmd", documentation=0)
+### Extract the example code chunks into an R script -----------------------------------------------
+# Note: purl flags are set in the global environment prison to extracting the code.
+purl_solutions <- FALSE 
+purl_example_code <- TRUE 
+knitr::purl(
+  "rmd_files/content.Rmd", 
+  documentation = 1, 
+  output = "example_code.R"
+)
+rm(purl_solutions, purl_example_code)
 
-# give it a more sensible name
-file.rename("content.R", "example_code.R")
 
-# extract solutions from content.Rmd into separate script
-purl_solutions <- TRUE # This means solutions will be included
-purl_example_code <- FALSE # This means code that isn't an exercise solution won't be included
-knitr::purl("rmd_files/content.Rmd", documentation=0)
-file.rename("content.R", "solutions.R")
+### Extract the solution code chunks into an R script ----------------------------------------------
+# Note: purl flags are set in the global environment prison to extracting the code.
+purl_solutions <- TRUE 
+purl_example_code <- FALSE 
+knitr::purl(
+  "rmd_files/content.Rmd", 
+  documentation = 1,
+  output = "solutions.R"
+)
+rm(purl_solutions, purl_example_code)
 
-# copy files that content.R and README.md call to the main folder
-file.copy("rmd_files/functions.R", ".", recursive = TRUE)
-
-file.copy("rmd_files/README_files", ".", recursive = TRUE)
